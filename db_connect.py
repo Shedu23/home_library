04d2682ddab3ba79b071bdb85a579d3c2052bc2c
Ghost <ghostcc18@gmail.com>
2019-06-14 00:10:51 +0300

    Project v0.05

--- db_connect.py
+++ db_connect.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import psycopg2
 from pprint import pprint
 
@@ -5,17 +7,69 @@ class DatabasConnector:
     def __init__(self):
         try:
             self.connection = psycopg2.connect(
-                "dbname='Own_library' user='postgres' host='localhost' password='123!qwe' port='5433' "
-            )
+                "dbname='Own_library' user='postgres' host='localhost' password='123!qwe' port='5433'")
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
+
         except:
-            pprint("Cannot connec to to database")
+            print("Cannot connect to to database")
 
-    #def crete_table(self):
+    def create_table(self):
+        #create_table_command = "CREATE TABLE pet(id integer, name varchar(100), age integer NOT NULL)"
+        create_table_command = '''
+                                    CREATE TABLE home_books(
+                                name	varchar(60),
+                                second_name	varchar(60),
+                                otchestvo	varchar(60),
+                                year_print	int,
+                                publishing	varchar(60),
+                                storage		varchar(100), 
+                                on_storage	varchar(40),
+                                quarcode_add	varchar(40),
+                                date_add	varchar(40),
+                                id	int);
 
-DatabasConnector()
-database_connection.isert_new_record()
+        '''
+
+        print("Inicialization ... Create table")
+        self.cursor.execute(create_table_command)
+
+    def drop_table(self):
+        try:
+            create_table_command = "DROP TABLE home_books"
+            self.cursor.execute(create_table_command)
+        except:
+            print("ОШИБКА:  таблица pet не существует")
+
+    def insert_data_to_table(self):
+        name = 'Борис'
+        #create_table_command1 = "Insert INTO home_books (name) VALUES ('%s')" % name
+        create_table_command = u'''
+                                INSERT INTO home_books (name, second_name, otchestvo, year_print, publishing, storage,
+                                 on_storage,quarcode_add, date_add, id)
+                                VALUES ('Борис', 'Ермаков', 'Петрович', 1995, 'ЭКСПО', 'в шкафу',
+                                 'первый шкаф, вторая полка, второй ряд', 'нет куаркода', '03-06-2019', 1);
+        '''
+        self.cursor.execute(create_table_command)
+
+    #def seach_data_in_table(self):
+
+if __name__== '__main__':
+    database_connection = DatabasConnector()
+    database_connection.drop_table()
+    database_connection.create_table()
+    #database_connection.drop_table()
+    database_connection.insert_data_to_table()
 
 
 #https://www.youtube.com/watch?v=Z9txOWCWMwA
+
+
+
+#INSERT INTO home_books (name, second_name, otchestvo, year_print, publishing, storage, on_storage,quarcode_add, date_add, id)
+# VALUES ('Борис', 'Ермаков', 'Петрович', 1995, 'ЭКСПО', 'в шкафу', 'первый шкаф, вторая полка, второй ряд', 'нет куаркода', '03-06-2019', 1);
+
+
+#INSERT INTO home_books (name, second_name, otchestvo, year_print, publishing, storage, on_storage,quarcode_add, date_add, id)
+ #VALUES ('Борис', 'Алексеев', 'Алексеевич', 1995, 'ЭКСПО', 'в шкафу', 'первый шкаф, вторая полка, второй ряд', 'нет куаркода', '03-08-2017', 3);
+#
